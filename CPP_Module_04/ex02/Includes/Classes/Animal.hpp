0a7db4bd80082dbf47241d75ff6adec0e98d0431
fatheddine-bicane <fatheddine04@gmail.com>
2025-11-20 17:27:45 +0100

    fix (ex02): provide copy constructor and override the asignment operator (orthodox canonical form)

--- CPP_Module_04/ex02/Includes/Classes/Animal.hpp
+++ CPP_Module_04/ex02/Includes/Classes/Animal.hpp
@@ -11,6 +11,8 @@ protected:
 public:
 	Animal();
 	Animal(const std::string& type);
+	Animal(const Animal& other);
+	Animal&	operator=(const Animal& other);
 	virtual void	makeSound() const = 0;
 	virtual std::string&	getType() const  = 0;
 	virtual ~Animal();
--- CPP_Module_04/ex02/Sources/Animal.cpp
+++ CPP_Module_04/ex02/Sources/Animal.cpp
@@ -9,10 +9,25 @@ Animal::Animal() : _type("typeless") {
 	std::cout << UNDERLINE
 		<< "Animal default constructor called!\n" RESET;
 }
-//
+
 Animal::Animal(const std::string& type) : _type(type) {
 	std::cout << UNDERLINE
 		<< "Animal parameterized constructor called!\n" RESET;
+}
+
+
+Animal::Animal(const Animal& other) : _type(other._type) {
+	std::cout << UNDERLINE
+		<< "Animal copy constructor called!\n" RESET;
+}
+
+
+Animal&	Animal::operator=(const Animal& other) {
+	if (this == &other) {
+		return *this;
+	}
+	_type = other._type;
+	return *this;
 }
 
 Animal::~Animal() {
