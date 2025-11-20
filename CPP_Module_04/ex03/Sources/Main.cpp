baaceea47be76cabf06507ea36e9b5ac8aaccfb8
fatheddine-bicane <fatheddine04@gmail.com>
2025-11-20 17:42:05 +0100

    feat (ex03): added debuging messages in the main

--- CPP_Module_04/ex03/Sources/Main.cpp
+++ CPP_Module_04/ex03/Sources/Main.cpp
@@ -12,6 +12,7 @@ int main()
 {
 	/*-------------------------------*/
 	// create a materia source
+	std::cout << BLACK BG_PURPLE "A materia source is ceated!\n" << RESET;
 	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
@@ -19,6 +20,8 @@ int main()
 
 	/*-------------------------------*/
 	// create two characters
+	std::cout << BLACK BG_PURPLE
+		<< "Two character joined the arena: Mike and Bob!\n" << RESET;
 	ICharacter* mike = new Character("mike");
 	ICharacter* bob = new Character("bob");
 	/*-------------------------------*/
@@ -26,11 +29,15 @@ int main()
 	/*-------------------------------*/
 	AMateria* tmp;
 	// mike equip materia's
+	std::cout << BLACK BG_PURPLE
+		<< "Mike is equiping materia's from the source!\n" << RESET;
 	tmp = src->createMateria("ice");
 	mike->equip(tmp);
 	tmp = src->createMateria("cure");
 	mike->equip(tmp);
 	// bob equip materia's
+	std::cout << BLACK BG_PURPLE
+		<< "Bob is equiping materia's from the source!\n" << RESET;
 	tmp = src->createMateria("ice");
 	bob->equip(tmp);
 	tmp = src->createMateria("cure");
@@ -39,6 +46,8 @@ int main()
 
 
 	/*-------------------------------*/
+	std::cout << BLACK BG_PURPLE
+		<< "Mike and Bob are about to start fighting!!!!\n" << RESET;
 	// now fight time !!!
 	mike->use(0, *bob);
 	bob->use(0, *mike);
@@ -47,15 +56,14 @@ int main()
 	/*-------------------------------*/
 
 	/*-------------------------------*/
+	std::cout << BLACK BG_PURPLE
+		<< "Fight is over, Mike and Bob are now unequiping there materia's\n" << RESET;
 	// mike and bob are unequiping there materia's
 	mike->unequip(0);
 	mike->unequip(1);
 	bob->unequip(0);
 	bob->unequip(1);
 	/*-------------------------------*/
-
-
-
 
 	delete bob;
 	delete mike;
