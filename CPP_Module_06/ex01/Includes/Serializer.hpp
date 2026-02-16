c4c95bfd463b4aebe73659906bef8ed05851feed
fatheddine-bicane <fatheddine04@gmail.com>
2026-02-16 17:08:36 +0000

    feat: ex01 implemented (reinterpret cast)

---
+++ CPP_Module_06/ex01/Includes/Serializer.hpp
@@ -0,0 +1,12 @@
+
+#include <stdint.h>
+#include "./Data.hpp"
+
+class Serializer {
+private:
+	Serializer();
+
+public:
+	static uintptr_t	serialize(Data* ptr);
+	static Data*	deserialize(uintptr_t raw);
+};
