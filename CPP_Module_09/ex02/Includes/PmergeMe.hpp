900d69288136045bfeddc07a513462ae571254cc
fatheddine-bicane <fatheddine04@gmail.com>
2026-03-04 02:18:59 +0000

    feat (ex02): binary inser the loser using the Jacobsthal sequence into the main chain

--- CPP_Module_09/ex02/Includes/PmergeMe.hpp
+++ CPP_Module_09/ex02/Includes/PmergeMe.hpp
@@ -12,7 +12,15 @@ private:
 
 	struct Element {
 		int	winner_value;
+		sequence	s;
+		int			index;
 		std::vector<Element>	tethered_losers;
+
+		// The overloaded operator< definition
+		bool operator<(const Element& other) const {
+			return this->winner_value < other.winner_value;
+		}
+		// TODO: might need a '<=' operator for inputs with equal elements
 	};
 
 	std::vector<Element>	_vSequence;
@@ -26,6 +34,10 @@ private:
 	// void	appendValue();
 	std::vector<Element>
 	pairAndCompareElements(std::vector<Element> input, Element& unpaired, bool& is_odd);
+
+
+	// apply algorithm
+	void updateJacobsthalBounds(int& stop, int& start);
 
 	std::vector<Element>	mergeIsertSort(std::vector<Element> input);
 
--- CPP_Module_09/ex02/Sources/PmergeMe.cpp
+++ CPP_Module_09/ex02/Sources/PmergeMe.cpp
@@ -1,4 +1,5 @@
 #include "../Includes/PmergeMe.hpp"
+#include <algorithm>
 #include <climits>
 #include <cstddef>
 #include <cstdlib>
@@ -47,12 +48,6 @@ void	PmergeMe::parseArguments(int argc, char** argv) {
 // INFO: applying the algorithm
 // -----------------------------------------------------------------------------------------
 
-// void	PmergeMe::appendValue() {
-// 	// std::vector<std::pair<int, std::pair<sequence, int> > >	tmp;
-// 	//
-// 	// tmp.push_back({10, {a, 0}});
-// }
-
 std::vector<PmergeMe::Element>
 PmergeMe::pairAndCompareElements(std::vector<Element> input, Element& unpaired, bool& is_odd) {
 	std::vector<Element>	winners;
@@ -90,61 +85,143 @@ PmergeMe::pairAndCompareElements(std::vector<Element> input, Element& unpaired, 
 	return winners;
 }
 
+
+
+void	PmergeMe::updateJacobsthalBounds(int& stop, int& start) {
+	stop++;
+	start++;
+    int next_start = start + 2 * stop;
+
+    stop = start - 1;
+    start = next_start - 1;
+}
+
+
 // function should take the new array (made of the winners)
-std::vector<PmergeMe::Element>	PmergeMe::mergeIsertSort(std::vector<Element> input) {
+std::vector<PmergeMe::Element>
+PmergeMe::mergeIsertSort(std::vector<Element> input) {
 
 	std::vector<Element>	winners;
 	std::vector<Element>	main_chain;
 	Element				unpaired;
 	bool	is_odd;
 
-	// create pairs and
+
+	// // return at the deepest recursion level since an array of size 1 is sorted!
+	if (input.size() <= 1) {
+		return input;
+	}
+
+	// create pairs and form a chain of the greatest of each pair
 	winners = pairAndCompareElements(input, unpaired, is_odd);
 
-	// recusive call until the input array size is 1 (wich is sorted)
-	// WARNING: should check agains 0 if the original input is empty
-	if (winners.size() > 1) {
-		// winners = pairAndCompareElements(winners, unpaired, is_odd);
-		main_chain = mergeIsertSort(winners);
-	}
-	
-	// return at the deepest recursion level since an array of size 1 is sorted!
-	if (winners.size() == 1) {
-		return winners;
-	}
+	// // recusive call until the input array size is 1 (wich is sorted)
+	main_chain = mergeIsertSort(winners);
 
 
 	// map the losers to the the main chaing possition
 	std::vector<Element>	pending_losers;
-
 	std::vector<Element>::iterator it = main_chain.begin();
 	std::vector<Element>::iterator end = main_chain.end();
-
 	for (int i = 0; it != end; it++, i++) {
 		Element tethered_loser = *(it->tethered_losers.end() - 1);
 		it->tethered_losers.erase(it->tethered_losers.end() - 1);
+		tethered_loser.s = b;
 		pending_losers.push_back(tethered_loser);
 	}
 
+	// if there is an unpaired element append it to the end of the losers
+	if (is_odd) {
+		unpaired.s = b;
+		pending_losers.push_back(unpaired);
+	}
+
+	// map the main chain element
+	for (size_t i = 0; i < main_chain.size(); i++) {
+		main_chain[i].s = a;
+		main_chain[i].index = i;
+	}
+
+
+
+
+
 
 	// append the first loser (b1) to the front of the main chain
+	// BUG: check if there are losers (rolling back at the first recursion level there should be no losers)
+	main_chain.insert(main_chain.begin(), *pending_losers.begin());
+
 
 
 	// using the jacobsthal sequence group elements to push and binary inset them
+	int	stop = 0;
+	int start = 2;
+
+	// normilize the index
+	if (start >= static_cast<int>(pending_losers.size())) {
+		start = pending_losers.size() - 1;
+	}
+
+	for (;;) {
+		// update index
+		int	index = start;
+
+		// isert the elements from start until we hit stop
+		while (index != stop) {
+
+			// ...
 
 
 
+			// find the upper bound in the main chain
+			std::vector<Element>::iterator upper_bound = main_chain.end();
+
+			std::vector<Element>::iterator it = main_chain.begin();
+			std::vector<Element>::iterator end = main_chain.end();
+
+			for (; it != end; it++) {
+				if (it->s == a && it->index == index) {
+					upper_bound = it;
+					break;
+				}
+			}
+
+			// the upper painding has no previous pair so the binary search should cover the entier chain
+			if (upper_bound == main_chain.end() && is_odd) {
+				upper_bound = main_chain.end() - 1;
+			}
+
+
+			// binary insert pending_losers[index] in the main chain
+			//	while respecting the upper bound
+			std::vector<Element>::iterator possition;
+			possition = std::lower_bound(main_chain.begin(), upper_bound, pending_losers[index]);
+			main_chain.insert(possition, pending_losers[index]);
+
+			// ...
+
+
+
+			index--;
+		}
+
+
+		//get the next index
+		updateJacobsthalBounds(stop, start);
+
+		// exit the loop if all losers where apended
+		if (stop >= static_cast<int>(pending_losers.size() - 1)) {
+			break;
+		}
+
+		// normilize the starting index and keep going ...
+		if (start >= static_cast<int>(pending_losers.size())) {
+			start = pending_losers.size() - 1;
+		}
+	}
 
 	return main_chain;
 }
-
-
-
-
-
-
-
-
 
 void	PmergeMe::applyAlgorithm(int argc, char** argv) {
 
@@ -160,7 +237,24 @@ void	PmergeMe::applyAlgorithm(int argc, char** argv) {
 
 
 	parseArguments(argc, argv);
-	mergeIsertSort(this->_vSequence);
+	std::vector<Element> sorted = mergeIsertSort(this->_vSequence);
+
+
+
+
+
+	std::cout << "sorting ..." << std::endl;
+	std::cout << "[";
+	for (int i = 1; i < argc; i++) {
+		std::cout << sorted[i - 1].winner_value;
+		if (i + 1 < argc) {
+			std::cout << ", ";
+		}
+	}
+	std::cout << "]" << std::endl;
+
+
+
 
 }
 // -----------------------------------------------------------------------------------------
